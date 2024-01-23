319984638ba598a109f6b45b7e4f57f19cf5e9b7
Arnav <48650781+arnavrneo@users.noreply.github.com>
2024-01-23 17:16:25 +0530

    deleted templates

--- url-shortener/servers/go/controllers/routes.go
+++ url-shortener/servers/go/controllers/routes.go
@@ -10,15 +10,12 @@ func LoadRoutes() *gin.Engine {
 	router := gin.Default()
 
 	router.LoadHTMLGlob("templates/*") // ../templates/* for testing
-
-	router.GET("/", loginPage)            // html serving
-	router.GET("/signupUser", signupPage) // html serving
+	
 	router.POST("/login", Login)
 	router.POST("/signup", signUp)
 	router.GET("/main", middleware.RequireAuth, handleForm)        // html serving
 	router.POST("/shorten", middleware.RequireAuth, handleShorten) // TODO: whether to merge this under /main
 	router.GET("/short/:id", handleRedirect)
-	router.GET("/validate", middleware.RequireAuth, validate) // middleware for protecting routes
 
 	return router
 }
