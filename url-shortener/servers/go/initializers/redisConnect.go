25c62c5c4d28222660b9551535a95a30ddeaf18d
Arnav <48650781+arnavrneo@users.noreply.github.com>
2024-01-26 22:43:43 +0530

    added go unit tests

--- url-shortener/servers/go/initializers/redisConnect.go
+++ url-shortener/servers/go/initializers/redisConnect.go
@@ -4,19 +4,20 @@ import (
 	"context"
 	"fmt"
 	"github.com/redis/go-redis/v9"
+	"os"
 )
 
 var Ctx = context.Background()
 
 func ConnectRedis() *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
+		Addr:     os.Getenv("REDIS_URI"),
+		Password: os.Getenv("REDIS_PASS"),
 		DB:       0,
 	})
 	_, err := rdb.Ping(Ctx).Result()
 	if err != nil {
-		fmt.Println("cannot connect redis client")
+		fmt.Println("cannot connect redis client", err)
 	} else {
 		fmt.Println("REDIS client: OK")
 	}
