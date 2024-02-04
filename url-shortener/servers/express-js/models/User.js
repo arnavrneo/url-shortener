56af63bccb296e01e339c6f75069aa5afd2b0ef5
Arnav <158455815+rarnavneo@users.noreply.github.com>
2024-02-05 01:22:23 +0530

    added bcrypt hashing

--- url-shortener/servers/express-js/models/User.js
+++ url-shortener/servers/express-js/models/User.js
@@ -1,4 +1,5 @@
 import {mongoose} from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -15,6 +16,19 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, "Please enter a password"],
     }
+})
+
+// fire a func after saving to db
+// userSchema.post('save', function (doc, next) {
+//
+//     next();
+// })
+
+// fire a func before saving to db
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
 })
 
 const User = mongoose.model("url_short_express_backend", userSchema);
